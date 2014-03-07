7364319e4c44443d38e223c74e60e41a290ddd49
Henrik Kjelsberg <hkjels@me.com>
2014-03-07 15:34:29 +0100

    replaces double-quotes inside values with single

--- index.js
+++ index.js
@@ -6,54 +6,45 @@
 var isArray = Array.isArray
   , keys = Object.keys
 
+/**
+ * Expose `CSV`.
+ */
+
+module.exports = CSV;
+
+/**
+ * Delimiters.
+ */
 
 CSV.CHAR_RETURN = 0xd;
 CSV.CHAR_NEWLINE = 0xa;
 CSV.DELIMITER = 0x2c;
 
-
-function head (a) {
-  return a[0];
-}
-
-function tail (a) {
-  return a[a.length -1];
-}
-
-function char (c) {
-  return 'number' === typeof c
-           ? String.fromCharCode.apply(null, arguments)
-           : c;
-}
-
 /**
  * Parses an array of objects to a CSV output
  */
 
-module.exports = CSV;
-function CSV (objects, opts) {
-  if ('object' !== typeof objects) throw new TypeError("expecting an array");
+function CSV(objects, opts){
+  if ('object' !== typeof objects) {
+    throw new TypeError('expecting an array');
+  }
+  opts = 'object' === typeof opts ? opts : {};
+  objects = isArray(objects) ? objects : [objects];
+  if (!objects.length) {
+    throw new Error('expecting at least one object');
+  }
 
-  opts = 'object' === typeof opts
-          ? opts
-          : {};
-
-  objects = isArray(objects)
-             ? objects
-             : [objects];
-
-  if (!objects.length) throw new Error("expecting at least one object");
-
-  var headers = keys(head(objects))
-    , buf = []
+  var headers = keys(head(objects));
+  var buf = [];
 
   while (objects.length) {
-    var lbuf = []
-      , object = objects.shift()
+    var lbuf = [];
+    var object = objects.shift();
 
-    for (var i = 0 ;i < headers.length; ++i) {
-      var header = headers[i]
+    for(var i = 0, c = headers.length; i < c; ++i){
+      var header = headers[i];
       if (lbuf.length) lbuf.push(char(CSV.DELIMITER));
+      object[header] = new String(object[header]).replace('"', "'");
       lbuf.push('"'+object[header]+'"');
     }
 
@@ -66,3 +57,10 @@ function CSV (objects, opts) {
           : buf.join('');
 }
 
+function head (a) { return a[0]; }
+function tail (a) { return a[a.length -1]; }
+function char (c) {
+  return 'number' === typeof c
+         ? String.fromCharCode.apply(null, arguments)
+         : c;
+}
